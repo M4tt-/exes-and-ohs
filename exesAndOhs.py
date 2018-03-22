c48e71313569ef17322ad3d749db932aab169c28
Matt Runyon <mtrunyon91@gmail.com>
2018-03-22 18:53:15 -0400

    exesAndOhs.py: Initial commit

---
+++ exesAndOhs.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 22 18:31:37 2018
+
+@author: mtrun
+"""
+
+board = ['', '', '', '','', '','','', '' , '']
+
+
+print('   |   |')
+
+print('  ' + board[7] + ' |  ' + board[8] + ' |  ' + board[9])
+
+print('   |   |')
+
+print('-----------')
+
+print('   |   |')
+
+print('  ' + board[4] + ' |  ' + board[5] + ' |  ' + board[6])
+
+print('   |   |')
+
+print('-----------')
+
+print('   |   |')
+
+print('  ' + board[1] + ' |  ' + board[2] + ' |  ' + board[3])
+
+print('   |   |')
