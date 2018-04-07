44fbd1b2d995f027918076be0f7bc8a4663b5c3c
Matt Runyon <mtrunyon91@gmail.com>
2018-04-07 17:46:35 -0400

    exesAndOhs.py: Made a function that draws the board, added placeholders for board positions

--- exesAndOhs.py
+++ exesAndOhs.py
@@ -5,27 +5,17 @@ Created on Thu Mar 22 18:31:37 2018
 @author: mtrun
 """
 
-board = ['', '', '', '','', '','','', '' , 'x']
+board = [' ', 'x', ' ', 'x',' ', ' ','0',' ', '0' , 'x']
 
-
-print('   |   |')
-
-print('  ' + board[7] + ' |  ' + board[8] + ' |  ' + board[9])
-
-print('   |   |')
-
-print('-----------')
-
-print('   |   |')
-
-print('  ' + board[4] + ' |  ' + board[5] + ' |  ' + board[6])
-
-print('   |   |')
-
-print('-----------')
-
-print('   |   |')
-
-print('  ' + board[1] + ' |  ' + board[2] + ' |  ' + board[3])
-
-print('   |   |')
+def draw_board(board):
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
